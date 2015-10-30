a221113c1076579216302414bf7d20be1790c241
Marques DeHerrera <session1@START-Coding-MBP-08.local>
2015-10-30 13:35:57 -0700

    This is the finished product of the Food Tracker Application

--- scripts.js
+++ scripts.js
@@ -1,27 +1,39 @@
 function enter() {
- var food = document.getElementById("food").value;
-    var newDiv = document.createElement("h2");
-    var newContent = document.createTextNode(food);
-    var currentDiv = document.getElementById("food1");
-    document.body.insertBefore(newDiv, currentDiv);
 
-    var description = document.getElementById("food").value;
-    var newDiv = document.createElement("h2");
-    var newContent = document.createTextNode(description);
-    var currentDiv = document.getElementById("food1");
-    document.body.insertBefore(newDiv, currentDiv);
+   var table = document.getElementById("myTable");
+   var row = table.insertRow(1);
+   var cell1 = row.insertCell(0);
+   var cell2 = row.insertCell(1);
+   var cell3 = row.insertCell(2);
+   var cell4 = row.insertCell(3);
 
-    var purchase = document.getElementById("food").value;
-    var newDiv = document.createElement("h2");
-    var newContent = document.createTextNode(purchase);
-    var currentDiv = document.getElementById("food1");
-    document.body.insertBefore(newDiv, currentDiv);
+   var food = document.getElementById("food").value;
+   var newContent = document.createTextNode(food);
+   cell1.appendChild(newContent);
 
-    var expiration = document.getElementById("food").value;
-    var newDiv = document.createElement("h2");
-    var newContent = document.createTextNode(expiration);
-    var currentDiv = document.getElementById("food1");
-    document.body.insertBefore(newDiv, currentDiv);
+   var description = document.getElementById("description").value;
+   var newContent2 = document.createTextNode(description);
+   cell2.appendChild(newContent2);
 
+   var purchase = document.getElementById("purchase").value;
+   var newContent3 = document.createTextNode(purchase);
+   cell3.appendChild(newContent3);
+
+   var expiration = document.getElementById("expiration").value;
+   var newContent4 = document.createTextNode(expiration);
+   cell4.appendChild(newContent4);
+
+if(expiration < purchase){
+   row.style.backgroundColor = "red";
 }
+   else{
+   row.style.backgroundColor = "white";
+}
+}
+
+
+
+
+
+
 
